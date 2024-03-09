76c1cc072cb63d524e19d5e72b5469d6ebda64ab
Rohit George Rarichan <rraricha@uci.edu>
2024-03-08 17:01:14 -0800

    Using the web api key that was acquired

--- OpenWeather.py
+++ OpenWeather.py
@@ -8,3 +8,39 @@
 # Rohit George Rarichan
 # rraricha@uci.edu
 # 36126645
+# API KEY - 65518d2062b2ced79bbb83249bd10638
+import urllib, json
+from urllib import request,error
+
+def _download_url(url_to_download: str) -> dict:
+    response = None
+    r_obj = None
+
+    try:
+        response = urllib.request.urlopen(url_to_download)
+        json_results = response.read()
+        r_obj = json.loads(json_results)
+
+    except urllib.error.HTTPError as e:
+        print('Failed to download contents of URL')
+        print('Status code: {}'.format(e.code))
+
+    finally:
+        if response != None:
+            response.close()
+    
+    return r_obj
+
+def main() -> None:
+    zip = "92697"
+    ccode = "US"
+    apikey = "65518d2062b2ced79bbb83249bd10638"
+    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
+
+    weather_obj = _download_url(url)
+    if weather_obj is not None:
+        print(weather_obj['weather'][0]['description'])
+
+
+if __name__ == '__main__':
+    main()
