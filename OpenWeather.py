7b0e9f2f168a7702d799f6e069f63f5464d70fc7
Rohit George Rarichan <rraricha@uci.edu>
2024-03-08 17:30:57 -0800

    Initializing and storing parts of the data retrieved from the url

--- OpenWeather.py
+++ OpenWeather.py
@@ -13,11 +13,20 @@ import urllib, json
 from urllib import request,error
 
 class OpenWeather:
-
-    def __init__(self, zipcode, ccode) -> None:
+    def __init__(self, zipcode:str, ccode:str) -> None:
         self.zipcode = zipcode
         self.ccode = ccode
         self.apikey = None
+        self.weather_data = None
+        self.temperature = None
+        self.high_temperature = None
+        self.low_temperature = None 
+        self.latitude = None 
+        self.longitude = None 
+        self.description = None 
+        self.humidity = None 
+        self.sunset = None 
+        self.sunrise = None 
     
     def set_apikey(self, apikey:str) -> None:
         ''' Sets the apikey required to make requests to a web API.:param apikey: 
@@ -31,6 +40,15 @@ class OpenWeather:
         try:
             url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zipcode},{self.ccode}&appid={self.apikey}"
             self.weather_data = _download_url(url)
+            self.temperature = self.weather_data['main']['temp']
+            self.high_temperature = self.weather_data['main']['temp_max']
+            self.low_temperature = self.weather_data['main']['temp_min']
+            self.longitude = self.weather_data['coord']['lon']
+            self.latitude = self.weather_data['coord']['lat']
+            self.description = self.weather_data['weather'][0]['description']
+            self.humidity = self.weather_data['main']['humidity']
+            self.sunset = self.weather_data['sys']['sunset']
+            self.sunrise = self.weather_data['sys']['sunrise']
         except:
             pass
 
@@ -57,12 +75,11 @@ def _download_url(url_to_download: str) -> dict:
 def main() -> None:
     zip = "92697"
     ccode = "US"
-    apikey = "65518d2062b2ced79bbb83249bd10638"
-    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
-
-    weather_obj = _download_url(url)
-    if weather_obj is not None:
-        print(weather_obj['weather'][0]['description'])
+    weather_apikey = "65518d2062b2ced79bbb83249bd10638"
+    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={weather_apikey}"
+    open_weather = OpenWeather(zip, ccode)
+    open_weather.set_apikey(weather_apikey)
+    open_weather.load_data()
 
 
 if __name__ == '__main__':
