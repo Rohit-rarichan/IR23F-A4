70070e603053cc9365a450ac95f5814af6f3b943
Rohit George Rarichan <rraricha@uci.edu>
2024-03-08 18:49:21 -0800

    Modifying a4.py for some additional requirements

--- a4.py
+++ a4.py
@@ -12,10 +12,13 @@ from LastFM import FM_main
 from OpenWeather import weather_main
 
 if __name__ == '__main__':
-    ans = input("Do you wish to use the Weather API or the LastFM API (type weather or FM)")
-    if ans == "weather":
-        weather_main()
-    elif ans == "FM":
-        FM_main()
-    else:
-        print("Invalid option")
+    loop = 'Y'
+    while loop != 'N':
+        ans = input("Do you wish to use the Weather API or the LastFM API (type weather or FM)")
+        if ans == "weather":
+            weather_main()
+        elif ans == "FM":
+            FM_main()
+        else:
+            print("Invalid option")
+        loop = input("Do you wish to continue Y/N: ")
